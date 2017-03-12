4655e26799df95848db5723f0c179a894ed32793
misha <mishahliba1@gmail.com>
2017-03-12 14:24:10 +0200

    css not works. kurwa

--- src/main/java/com/bovt/InitialApplication.java
+++ src/main/java/com/bovt/InitialApplication.java
@@ -1,11 +1,21 @@
 package com.bovt;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class InitialApplication {
+@EnableAutoConfiguration
+@EnableWebMvc
+public class InitialApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InitialApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(InitialApplication.class, args);
 	}
---
+++ src/main/java/com/bovt/controllers/Index.java
@@ -0,0 +1,17 @@
+package com.bovt.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by beerman on 12.03.2017.
+ */
+@Controller
+public class Index {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
